8e51e598da97e558c9f21b237e20cebfb5f4b707
Zach Shannon <shannonz2020@gmail.com>
2019-11-14 09:54:47 -0800

    added some stuff

---
+++ src/PushCounterAgain.java
@@ -0,0 +1,51 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class PushCounterAgain extends Application {
+    private int count;
+    private Text countText;
+
+    @Override
+    public void start(Stage stage) {
+        count = 0;
+        countText = new Text("Pushes: " + count);
+
+        Button up = new Button("Increase!");
+        up.setOnAction(this::processButtonPressInc);
+
+        Button down = new Button("Decrease!");
+        down.setOnAction(this::processButtonPressDec);
+
+        FlowPane pane = new FlowPane(up, countText, down);
+        pane.setAlignment(Pos.CENTER);
+        pane.setHgap(20);
+        pane.setStyle("-fx-background-color: cyan");
+
+        Scene scene = new Scene(pane, 300, 100);
+
+        stage.setTitle("Push Counter");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void processButtonPressInc(ActionEvent event) {
+        // Counts number of button clicks and shows the result on a label
+        count++;
+        countText.setText("Pushes: " + count);
+
+    }
+
+    private void processButtonPressDec(ActionEvent event) {
+        // Counts number of button clicks and shows the result on a label
+        count--;
+        countText.setText("Pushes: " + count);
+
+    }
+
+}
